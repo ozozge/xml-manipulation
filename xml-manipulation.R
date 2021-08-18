3920f5be7436978230c43c32f11d171a10fbcdbb
F. Ozge Ozturk <ozozge@gmail.com>
2021-08-18 17:57:08 -0400

    Add files via upload

---
+++ xml-manipulation.R
@@ -0,0 +1,23 @@
+library(XML)
+
+
+setwd("folder")
+
+listF <- list.files(path = ".", pattern = "*.xml")
+
+for (j in 1:(length(listF))){
+  namef<-listF[j]
+  input<-xmlInternalTreeParse(file = namef)
+  orders2<-getNodeSet(input, "/folder1/subfolder/parent node name")
+  top<-newXMLNode("a",attrs = c(b="c",d="e"),newXMLNode("f","g"),parent = orders2)
+  #output: 
+          #      <a b="c" d="e">
+          #        <f>g</f>
+          #      </a>
+  saveXML(input,namef)
+  
+}
+
+
+
+#rm(list = ls())
