3d35ff7d8c854b76e0443e7c4fdae40fdc0032ec
F. Ozge Ozturk <ozozge@gmail.com>
2021-08-20 10:49:19 -0400

    Update xml-manipulation.R

--- xml-manipulation.R
+++ xml-manipulation.R
@@ -10,7 +10,7 @@ for (j in 1:(length(listF))){
   input<-xmlInternalTreeParse(file = namef)
   orders2<-getNodeSet(input, "/folder1/subfolder/parent node name")
   top<-newXMLNode("a",attrs = c(b="c",d="e"),newXMLNode("f","g"),parent = orders2)
-  #output: 
+  #output under parent node: 
           #      <a b="c" d="e">
           #        <f>g</f>
           #      </a>
